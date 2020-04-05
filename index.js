1adee49752cee5f3796f2c8d087f74f6d7d269f5
cheerful-response-5821 <bryan.nguyen2494@gmail.com>
2020-04-05 04:13:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`;
 }
 console.log(happyHolidaysTo('hoang'));
+
+function happyCustomHolidayTo(holiday,name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+console.log(happyCustomHolidayTo("New Year","Hoang"));
